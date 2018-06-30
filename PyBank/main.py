43a463f8f1a78aeab246708c52dc44ac1421a40d
simpson12013 <simpson12013@gmail.com>
2018-06-30 09:44:54 -0500

    working code no txt output

---
+++ PyBank/main.py
@@ -0,0 +1,109 @@
+#import dependencies
+import os
+import csv
+
+
+
+csvpath = 'budget_data.csv'
+
+
+#COUNT OF ROWS
+with open(csvpath, newline='') as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    #print(csvreader)
+
+    csv_header = next(csvreader)
+    #print(f"CSV HEADER: {csv_header}")
+
+    #for row in csvreader:
+    #    print(row)
+
+
+
+
+        #list comprehesion THIS ONE
+    row_count = sum(1 for row in csvreader)
+    #print (row_count)
+
+        #.append()
+    # row_count = []
+    # for row in csvreader:
+    #     row_count.append(row)
+
+        #list()
+    # rows = list(csvreader)
+    # row_count = len(rows)
+    # for row in rows:
+    #     print (row_count)
+
+#TOTAL NET
+with open(csvpath, newline='') as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    #print(csvreader)
+
+    csv_header = next(csvreader)
+
+        #net.append()
+    # net=[]
+    # for row in csvreader:
+    #     net.append(2)
+    # print (net)
+    # nettotal = sum(net)
+    # print (nettotal)
+
+        #list comprehension
+    net = sum(int(row[1]) for row in csvreader)
+    #print (net)
+
+#Average Change
+with open(csvpath, newline='') as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    #print(csvreader)
+
+    csv_header = next(csvreader)
+
+        #net.append()
+    # net=[]
+    # for row in csvreader:
+    #     net.append(2)
+    # print (net)
+    # nettotal = sum(net)
+    # print (nettotal)
+
+        #list comprehension
+    change = []
+    changem = []
+    d = 1
+    for row in csvreader:
+        if d == 1:
+            i = row[1]
+            changem.append(row[0])
+            d = d + 1
+        else:
+            change.append(int(row[1])-int(i))
+            i = row[1]
+            changem.append(row[0])
+
+    avgchange = (sum(change)/len(change))
+    
+    maxchange = max(change)
+    maxchangeindex = change.index(maxchange)
+    maxmonth = changem[maxchangeindex + 1]
+    
+    minchange = min(change)
+    minchangeindex = change.index(minchange)
+    minmonth = changem[minchangeindex + 1]
+    
+    print ("Financial Analysis")
+    print ("--------------------------")
+    print ("Total Months: " + str(row_count))
+    print ("Total Profit/Losses: $" + str(net))
+    print ("Average Change: $" + str(avgchange))
+    print ("Greatest Increase in Profits: " + maxmonth + " ($" + str(maxchange) + ")")
+    print ("Greatest Decrease in Profits: " + minmonth + " ($" + str(minchange) + ")")
---
+++ PyPoll/main.py
@@ -0,0 +1,59 @@
+import os
+import csv
+
+csvpath = 'election_data.csv'
+
+with open(csvpath, newline = '') as csvfile:
+   csvreader = csv.reader(csvfile, delimiter=',')
+
+   csv_header = next(csvreader)
+
+   row_count = sum(1 for row in csvreader)
+
+
+
+with open(csvpath, newline = '') as csvfile:
+   csvreader = csv.reader(csvfile, delimiter=',')
+
+   csv_header = next(csvreader)
+
+   candidates = []
+
+   for row in csvreader:
+       candidate = row[2].split()
+       for word in candidate:
+           if word not in candidates:
+               candidates.append(word)
+
+with open(csvpath, newline = '') as csvfile:
+   csvreader = csv.reader(csvfile, delimiter=',')
+
+   csv_header = next(csvreader)
+   canone = 0
+   cantwo = 0
+   canthree = 0
+   canfour = 0
+   for row in csvreader:
+       if candidates[0] == row[2]:
+           canone = canone + 1
+       if candidates[1] == row[2]:
+           cantwo = cantwo + 1
+       if candidates[2] == row[2]:
+           canthree = canthree + 1
+       if candidates[3] == row[2]:
+           canfour = canfour + 1
+canvotes = [canone, cantwo, canthree, canfour]
+winnerfinder = canvotes.index(max(canvotes))
+winner = candidates[winnerfinder]
+
+print ("Election Results")
+print ("---------------------------")
+print ("Total Votes: " + str(row_count))
+print ("---------------------------")
+print (candidates[0] + ":" + " {0:.0%} ".format(canone/row_count) + "(" + str(canone) + ")")
+print (candidates[1] + ":" + " {0:.0%} ".format(cantwo/row_count) + "(" + str(cantwo) + ")")
+print (candidates[2] + ":" + " {0:.0%} ".format(canthree/row_count) + "(" + str(canthree) + ")")
+print (candidates[3] + ":" + " {0:.0%} ".format(canfour/row_count) + "(" + str(canfour) + ")")
+print ("---------------------------")
+print ("Winner: " + winner)
+print ("---------------------------")
